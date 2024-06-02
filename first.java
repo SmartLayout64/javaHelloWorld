00e7e6c8c4b2eef571e3ba46bf687454d02a6da8
SmartLayout <114115793+SmartLayout64@users.noreply.github.com>
2024-06-02 10:19:37 -0400

    Add files via upload

---
+++ first.java
@@ -0,0 +1,8 @@
+package Java;
+
+class first {
+    public static void main(String args[]) // this function is automatically called by java
+    {
+        System.out.println("Hello World!"); // print "Hello World!"
+    }
+}
